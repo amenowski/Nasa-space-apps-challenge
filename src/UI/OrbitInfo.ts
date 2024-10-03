c8dee9a024208c6311a7db416e6e2aaa89ea7c3c
amenowski <contactserus@gmail.com>
2024-10-03 20:39:37 +0200

    create handlers to open and close creadits ui

--- src/UI/OrbitInfo.ts
+++ src/UI/OrbitInfo.ts
@@ -1,94 +1,91 @@
 import CelestialBody from "../components/CelestialBody";
 
 export default class OrbitInfo {
-    private mainContainer: HTMLDivElement | null = null;
-    private elementsContainer: HTMLDivElement | null = null;
-    private objectName: HTMLParagraphElement | null = null;
-    private moreInfoBtn: HTMLDivElement | null = null;
-    private units: Map<string, HTMLParagraphElement> = new Map();
-    private numbers: Map<string, HTMLParagraphElement> = new Map();
+  private mainContainer: HTMLDivElement | null = null;
+  private elementsContainer: HTMLDivElement | null = null;
+  private objectName: HTMLParagraphElement | null = null;
+  private moreInfoBtn: HTMLDivElement | null = null;
+  private units: Map<string, HTMLParagraphElement> = new Map();
+  private numbers: Map<string, HTMLParagraphElement> = new Map();
 
-    constructor() {
-        this.mainContainer = document.querySelector(".UI .object-info");
+  constructor() {
+    this.mainContainer = document.querySelector(".UI .object-info");
 
-        if (!this.mainContainer) return;
+    if (!this.mainContainer) return;
 
-        this.elementsContainer = document.querySelector(
-            ".UI .orbital-elements"
-        );
-        this.moreInfoBtn = this.mainContainer.querySelector(".main-info");
+    this.elementsContainer = document.querySelector(".UI .orbital-elements");
+    this.moreInfoBtn = this.mainContainer.querySelector(".main-info");
 
-        this.objectName = this.mainContainer.querySelector(
-            ".main-info .object-name"
-        );
+    this.objectName = this.mainContainer.querySelector(
+      ".main-info .object-name"
+    );
 
-        this.getAllElements();
-    }
+    this.getAllElements();
+  }
 
-    public setEventListeners(): void {
-        if (this.moreInfoBtn)
-            this.moreInfoBtn.addEventListener("click", () => {
-                this.mainContainer!.classList.toggle("extended");
-            });
-    }
+  public setEventListeners(): void {
+    if (this.moreInfoBtn)
+      this.moreInfoBtn.addEventListener("click", () => {
+        this.mainContainer!.classList.toggle("extended");
+      });
+  }
 
-    public displayInfo(object: CelestialBody): void {
-        const orbitData = object.getOrbit();
+  public displayInfo(object: CelestialBody): void {
+    const orbitData = object.getOrbit();
 
-        this.numbers.get("semiMajor")!.innerText =
-            typeof orbitData.semiMajorAxis == "string"
-                ? parseFloat(orbitData.semiMajorAxis).toPrecision(8)
-                : orbitData.semiMajorAxis.toPrecision(8);
+    this.numbers.get("semiMajor")!.innerText =
+      typeof orbitData.semiMajorAxis == "string"
+        ? parseFloat(orbitData.semiMajorAxis).toPrecision(8)
+        : orbitData.semiMajorAxis.toPrecision(8);
 
-        this.units.get("semiMajor")!.innerText = "au";
+    this.units.get("semiMajor")!.innerText = "au";
 
-        this.numbers.get("eccentricity")!.innerText =
-            orbitData.eccentricity.toPrecision(8);
+    this.numbers.get("eccentricity")!.innerText =
+      orbitData.eccentricity.toPrecision(8);
 
-        this.numbers.get("inclination")!.innerText = (
-            orbitData.inclination *
-            (180 / Math.PI)
-        ).toPrecision(8);
-        this.units.get("inclination")!.innerText = "deg";
+    this.numbers.get("inclination")!.innerText = (
+      orbitData.inclination *
+      (180 / Math.PI)
+    ).toPrecision(8);
+    this.units.get("inclination")!.innerText = "deg";
 
-        this.numbers.get("longAn")!.innerText = (
-            orbitData.ascendingNode *
-            (180 / Math.PI)
-        ).toPrecision(8);
-        this.units.get("longAn")!.innerText = "deg";
+    this.numbers.get("longAn")!.innerText = (
+      orbitData.ascendingNode *
+      (180 / Math.PI)
+    ).toPrecision(8);
+    this.units.get("longAn")!.innerText = "deg";
 
-        this.numbers.get("argPeri")!.innerText = (
-            orbitData.argPeri *
-            (180 / Math.PI)
-        ).toPrecision(8);
-        this.units.get("argPeri")!.innerText = "deg";
+    this.numbers.get("argPeri")!.innerText = (
+      orbitData.argPeri *
+      (180 / Math.PI)
+    ).toPrecision(8);
+    this.units.get("argPeri")!.innerText = "deg";
 
-        this.numbers.get("period")!.innerText = orbitData.period.toPrecision(8);
-        this.units.get("period")!.innerText = "y";
+    this.numbers.get("period")!.innerText = orbitData.period.toPrecision(8);
+    this.units.get("period")!.innerText = "y";
 
-        this.numbers.get("type")!.innerText = object.type;
-        if (this.objectName) this.objectName.innerText = object.name;
-    }
+    this.numbers.get("type")!.innerText = object.type;
+    if (this.objectName) this.objectName.innerText = object.name;
+  }
 
-    public showContainer(): void {
-        this.mainContainer?.classList.add("visible");
-    }
+  public showContainer(): void {
+    this.mainContainer?.classList.add("visible");
+  }
 
-    public hideContainer(): void {
-        this.mainContainer?.classList.remove("visible");
-    }
+  public hideContainer(): void {
+    this.mainContainer?.classList.remove("visible");
+  }
 
-    private getAllElements(): void {
-        if (!this.elementsContainer) return;
+  private getAllElements(): void {
+    if (!this.elementsContainer) return;
 
-        const elements = this.elementsContainer.querySelectorAll("li");
+    const elements = this.elementsContainer.querySelectorAll("li");
 
-        elements.forEach((elem) => {
-            const numberField =
-                elem.querySelector<HTMLParagraphElement>(".number");
-            if (numberField) this.numbers.set(elem.className, numberField);
-            const unit = elem.querySelector<HTMLParagraphElement>(".unit");
-            if (unit) this.units.set(elem.className, unit);
-        });
-    }
+    elements.forEach((elem) => {
+      const numberField = elem.querySelector<HTMLParagraphElement>(".number");
+      if (numberField) this.numbers.set(elem.className, numberField);
+      const unit = elem.querySelector<HTMLParagraphElement>(".unit");
+      if (unit) this.units.set(elem.className, unit);
+    });
+  }
 }
--- src/core/UI.ts
+++ src/core/UI.ts
@@ -1,112 +1,113 @@
 import CelestialBody from "../components/CelestialBody";
 import SolarSystem from "../components/SolarSystem";
+import Credits from "../UI/Credits";
 import OrbitInfo from "../UI/OrbitInfo";
 import SearchBar from "../UI/SearchBar";
 import TimeSlider from "../UI/TimeSlider";
 import { getMonthShortName } from "../utils/DateConverter";
 
 export class UI {
-    private date: HTMLDivElement | null = null;
-    private clock: HTMLDivElement | null = null;
-    private liveBtn: HTMLDivElement | null = null;
-    private moveToDefaultBtn: HTMLDivElement | null = null;
-    private searchBar: SearchBar;
-    private timeSlider: TimeSlider;
-    private orbitInfo: OrbitInfo;
-    private solarSystem: SolarSystem;
+  private date: HTMLDivElement | null = null;
+  private clock: HTMLDivElement | null = null;
+  private liveBtn: HTMLDivElement | null = null;
+  private moveToDefaultBtn: HTMLDivElement | null = null;
+  private searchBar: SearchBar;
+  private timeSlider: TimeSlider;
+  private orbitInfo: OrbitInfo;
+  private solarSystem: SolarSystem;
+  private credits: Credits;
 
-    constructor(solarSystem: SolarSystem) {
-        this.clock = document.querySelector<HTMLDivElement>(
-            ".UI .time-slider .info .clock"
-        );
-        this.date = document.querySelector<HTMLDivElement>(
-            ".UI .time-slider .info .date"
-        );
-        this.liveBtn = document.querySelector(
-            ".UI .time-slider .info .live-btn"
-        );
+  constructor(solarSystem: SolarSystem) {
+    this.clock = document.querySelector<HTMLDivElement>(
+      ".UI .time-slider .info .clock"
+    );
+    this.date = document.querySelector<HTMLDivElement>(
+      ".UI .time-slider .info .date"
+    );
+    this.liveBtn = document.querySelector(".UI .time-slider .info .live-btn");
 
-        this.moveToDefaultBtn = document.querySelector(".UI .move-to-default");
+    this.moveToDefaultBtn = document.querySelector(".UI .move-to-default");
 
-        this.timeSlider = new TimeSlider();
-        this.searchBar = new SearchBar(solarSystem);
-        this.orbitInfo = new OrbitInfo();
+    this.timeSlider = new TimeSlider();
+    this.searchBar = new SearchBar(solarSystem);
+    this.orbitInfo = new OrbitInfo();
+    this.credits = new Credits();
 
-        this.setEventListeners();
-        this.solarSystem = solarSystem;
+    this.setEventListeners();
+    this.solarSystem = solarSystem;
+  }
+
+  public showResetPosition(): void {
+    if (this.moveToDefaultBtn) this.moveToDefaultBtn.classList.add("show");
+  }
+
+  public hideResetPosition(): void {
+    if (this.moveToDefaultBtn) this.moveToDefaultBtn.classList.remove("show");
+  }
+
+  public noLive(): void {
+    if (this.liveBtn) {
+      this.liveBtn.classList.add("no-live");
     }
+  }
 
-    public showResetPosition(): void {
-        if (this.moveToDefaultBtn) this.moveToDefaultBtn.classList.add("show");
+  public updateTimelineInfo(date: Date): void {
+    let hours: string | number = date.getHours();
+    let minutes: string | number = date.getMinutes();
+    let seconds: string | number = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    // console.log(`${hours}:${minutes}:${seconds}`);
+    if (this.clock) this.clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+    let day = date.getDate();
+    let month = getMonthShortName(date.getMonth());
+    let year = date.getFullYear();
+
+    if (this.date) this.date.innerText = `${day} ${month}, ${year}`;
+  }
+
+  public displayResult(results: string[]): void {
+    this.searchBar.displaySearchResult(results);
+  }
+
+  public showOrbitInfo(object: CelestialBody): void {
+    this.orbitInfo.displayInfo(object);
+    this.orbitInfo.showContainer();
+  }
+
+  public hideOrbitInfo(): void {
+    this.orbitInfo.hideContainer();
+  }
+
+  private setEventListeners(): void {
+    this.timeSlider.setEventListeners();
+    this.searchBar.setEventListeners();
+    this.orbitInfo.setEventListeners();
+    this.credits.setEventListeners();
+
+    if (this.liveBtn)
+      this.liveBtn.addEventListener("click", () => {
+        this.solarSystem.setLiveDate();
+        this.live();
+        this.timeSlider.reset();
+      });
+
+    if (this.moveToDefaultBtn)
+      this.moveToDefaultBtn.addEventListener("click", () => {
+        this.solarSystem.resetCamPosition();
+      });
+  }
+
+  public hideSearchBar(): void {
+    this.searchBar.clearResult();
+  }
+
+  private live(): void {
+    if (this.liveBtn) {
+      this.liveBtn.classList.remove("no-live");
     }
-
-    public hideResetPosition(): void {
-        if (this.moveToDefaultBtn)
-            this.moveToDefaultBtn.classList.remove("show");
-    }
-
-    public noLive(): void {
-        if (this.liveBtn) {
-            this.liveBtn.classList.add("no-live");
-        }
-    }
-
-    public updateTimelineInfo(date: Date): void {
-        let hours: string | number = date.getHours();
-        let minutes: string | number = date.getMinutes();
-        let seconds: string | number = date.getSeconds();
-        if (hours < 10) hours = "0" + hours;
-        if (minutes < 10) minutes = "0" + minutes;
-        if (seconds < 10) seconds = "0" + seconds;
-
-        // console.log(`${hours}:${minutes}:${seconds}`);
-        if (this.clock) this.clock.innerText = `${hours}:${minutes}:${seconds}`;
-
-        let day = date.getDate();
-        let month = getMonthShortName(date.getMonth());
-        let year = date.getFullYear();
-
-        if (this.date) this.date.innerText = `${day} ${month}, ${year}`;
-    }
-
-    public displayResult(results: string[]): void {
-        this.searchBar.displaySearchResult(results);
-    }
-
-    public showOrbitInfo(object: CelestialBody): void {
-        this.orbitInfo.displayInfo(object);
-        this.orbitInfo.showContainer();
-    }
-
-    public hideOrbitInfo(): void {
-        this.orbitInfo.hideContainer();
-    }
-
-    private setEventListeners(): void {
-        this.timeSlider.setEventListeners();
-        this.searchBar.setEventListeners();
-        this.orbitInfo.setEventListeners();
-
-        if (this.liveBtn)
-            this.liveBtn.addEventListener("click", () => {
-                this.solarSystem.setLiveDate();
-                this.live();
-                this.timeSlider.reset();
-            });
-
-        if (this.moveToDefaultBtn)
-            this.moveToDefaultBtn.addEventListener("click", () => {
-                this.solarSystem.resetCamPosition();
-            });
-    }
-
-    public hideSearchBar(): void {
-        this.searchBar.clearResult();
-    }
-
-    private live(): void {
-        if (this.liveBtn) {
-            this.liveBtn.classList.remove("no-live");
-        }
-    }
+  }
 }
